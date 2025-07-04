c369f7df0207df72025e50a216d4fa0c4c9905f6
Umberto Albano <umberto.albano@planet.de>
2025-07-04 07:21:40 +0200

    fix: Use docker compose plugin in update.sh
    Accidentally I use the old docker-compose tool in the update.sh.
    This Tool is deprecated and should not be used. docker compose is now a
    plugin in docker and this should be used.
    
    This Commit change the update.sh to this desired behavior.

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,12 +11,12 @@ set -eu
 function update_docker_containers() {
 
 	# I separate pull and rebuild into two steps in order to avoid unclear states.
-	docker-compose pull
+	docker compose pull
 	# --force-recreate ensures that all containers are restarted in both ways.
 	# way. Assuming the configuration has changed, in this case you need to restart the container
 	# to make the change available and docker compose up will only restart the container.
 	# (and restart) the container when the container changes.
-	docker-compose up -d --force-recreate --remove-orphans
+	docker compose up -d --force-recreate --remove-orphans
 }
 
 git pull --rebase
